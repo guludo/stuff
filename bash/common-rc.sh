674efa2cf82be356dc679130a9d24d22f4e7fff8
Gustavo Sousa <gustavo.sousa@intel.com>
2025-09-04 11:13:34 -0300

    bash: Set EDITOR variable
    Set EDITOR variable to nvim if nvim is available.

--- bash/common-rc.sh
+++ bash/common-rc.sh
@@ -3,3 +3,8 @@ export MANWIDTH=80
 if type -t nvim >/dev/null; then
     export MANPAGER='nvim +Man! +normal\ gO'
 fi
+
+# Editor settings
+if type -t nvim >/dev/null; then
+    export EDITOR=nvim
+fi
